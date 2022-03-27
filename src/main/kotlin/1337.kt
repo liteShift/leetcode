8b273048a1c86059ac9912c9f36c1dbe14e0ead8
Taylor Ellett <teellett@gmail.com>
2022-03-27 11:38:57 -0500

    add completed problem 1337

--- src/main/kotlin/1337.kt
+++ src/main/kotlin/1337.kt
@@ -1,20 +1,54 @@
 class Solution {
+    /**
+     * m x n binary matrix where for each row, all 1s will appear before all 0s
+     *
+     * a row is weaker than another row if any of the following are true:
+     *      the number of soldiers in row i is less than the number of soldiers in row j
+     *      both rows have the same number of soldiers and i < j
+     *
+     *      the row's index increases its number of sliders
+     *
+     * Return the indices of the k weakest rows in the matrix ordered from weakest to strongest
+     */
     fun kWeakestRows(mat: Array<IntArray>, k: Int): IntArray {
-        // binary matrix m x n
-        // the 1s appear before the 0s in each row (to the left)
+        val scoreMap = mutableMapOf<Int, MutableList<Int>>()
+        mat.forEachIndexed { rowIndex, row ->
+            val score = scoreRow(row)
+            scoreMap[score]?.add(rowIndex) ?: with(mutableListOf(rowIndex)) {
+                scoreMap[score] = this
+            }
+        }
 
-        // a row is weaker than another row if the one of the following are true:
-
-        /**
-         * the number of soldiers in row i is less than the number of soldiers in row j
-         * both rows have the same number of soldiers and i < j
-         *
-         * the row's index increases its number of sliders
-         *
-         * Return the indices of the k weakest rows in the matrix ordered from weakest to strongest
-         */
-
+        val kWeakestRows = mutableListOf<Int>()
+        for (i in 0..mat[0].size) {
+            if (kWeakestRows.size == k) break
+            scoreMap[i]?.take(k - kWeakestRows.size)?.forEach {
+                kWeakestRows.add(it)
+            }
+        }
+        return kWeakestRows.toIntArray()
     }
 
-    fun scoreRow(row: IntArray)
+    private fun scoreRow(row: IntArray): Int {
+        return row.count { it == 1 }
+    }
+}
+
+fun main() {
+    val mat = arrayOf(
+        arrayOf(1, 1, 0, 0, 0),
+        arrayOf(1, 1, 1, 1, 0),
+        arrayOf(1, 0, 0, 0, 0),
+        arrayOf(1, 1, 0, 0, 0),
+        arrayOf(1, 1, 1, 1, 1)
+    ).map {
+        it.toIntArray()
+    }.toTypedArray()
+
+    val solution = Solution()
+    val kWeakestRows = solution.kWeakestRows(mat, 3)
+    kWeakestRows.forEach {
+        println(it)
+    }
+
 }
